44bfdfb7e6a542c9d164c58fca021d83de21b16f
prakor <warunyoo084@gmail.com>
2024-01-17 09:24:35 +0700

    Update candidate.contrller.js

--- controllers/candidate/candidate.contrller.js
+++ controllers/candidate/candidate.contrller.js
@@ -1,6 +1,76 @@
 const bcrypt = require("bcrypt");
 const dayjs = require("dayjs");
+const {google} = require("googleapis");
+const req = require("express/lib/request.js");
 const {candidate,validateCadidate} = require("../../models/candidate/candidate.model")
+const CLIENT_ID = process.env.GOOGLE_DRIVE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_DRIVE_CLIENT_SECRET;
+const REDIRECT_URI = process.env.GOOGLE_DRIVE_REDIRECT_URI;
+const REFRESH_TOKEN = process.env.GOOGLE_DRIVE_REFRESH_TOKEN;
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+const drive = google.drive({
+  version: "v3",
+  auth: oauth2Client,
+});
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+    // console.log(file.originalname);
+  },
+});
+async function generatePublicUrl(res) {
+  console.log("generatePublicUrl");
+  try {
+    const fileId = res;
+    await drive.permissions.create({
+      fileId: fileId,
+      requestBody: {
+        role: "reader",
+        type: "anyone",
+      },
+    });
+    const result = await drive.files.get({
+      fileId: fileId,
+      fields: "webViewLink, webContentLink",
+    });
+    console.log(result.data);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({message: "Internal Server Error"});
+  }
+}
+//update image
+async function uploadFileCreate(req, res, {i, reqFiles}) {
+  if (!req[i]) {
+    console.error("Invalid value for 'i'");
+    return res.status(400).send({ message: "Invalid value for 'i'" });
+  }
+  const filePath = req[i].path;
+  let fileMetaData = {
+    name: req.originalname,
+    parents: [process.env.GOOGLE_DRIVE_IMAGE_PRODUCT],
+  };
+  let media = {
+    body: fs.createReadStream(filePath),
+  };
+  try {
+    const response = await drive.files.create({
+      resource: fileMetaData,
+      media: media,
+    });
+
+    generatePublicUrl(response.data.id);
+    reqFiles.push(response.data.id);
+  } catch (error) {
+    res.status(500).send({message: "Internal Server Error"});
+  }
+}
+
 
 exports.create = async (req, res) => {
     try {
